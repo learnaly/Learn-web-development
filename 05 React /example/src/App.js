a867bb931a89d949e5db7db9dddb99d00e6b7ffb
lazar-qoolpage <lazareric2@gmail.com>
2021-06-22 20:13:35 +0200

    React poart 2 examples

--- 05 React /example/src/App.js
+++ 05 React /example/src/App.js
@@ -1,30 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 
 import './App.css';
 
-import Cat from './Cat';
-
 function App() {
+  const [text, setText] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <input
+        onChange={event => {
+          setText(event.target.value);
+        }}
+      />
 
-        <Cat
-
-        />
-      </header>
+      <h1>{text}</h1>
+      <h2>{text}</h2>
+      <h3>{text}</h3>
     </div>
   );
 }
